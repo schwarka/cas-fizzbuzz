e4dadac6d306023b2e44bd7f0447b968f8d738fc
schwarka <schwartz.ka@gmail.com>
2016-11-09 21:10:03 -0500

    Red - will not compile, missing method

--- src/test/scala/fizzBuzzSpec.scala
+++ src/test/scala/fizzBuzzSpec.scala
@@ -1,4 +1,8 @@
 import org.scalatest.{FlatSpec, Matchers}
 
 class FizzBuzzSpec extends FlatSpec with Matchers {
+
+	"A FizzBuzz" should "print 1" in {
+		FizzBuzz.calculate(1) should be ("1")
+	}
 }
