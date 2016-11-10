030ea43b8195057f524be71e1220e693f4d96e91
schwarka <schwartz.ka@gmail.com>
2016-11-09 21:35:16 -0500

    Red - second test failing

--- src/test/scala/fizzBuzzSpec.scala
+++ src/test/scala/fizzBuzzSpec.scala
@@ -5,4 +5,8 @@ class FizzBuzzSpec extends FlatSpec with Matchers {
 	"A FizzBuzz" should "print 1" in {
 		FizzBuzz.calculate(1) should be ("1")
 	}
+
+	it should "print 2" in {
+		FizzBuzz.calculate(2) should be ("2")
+	}
 }
